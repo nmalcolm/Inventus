95cf17544031845f3d61ab05ca3aea655bd38167
Nathan Malcolm <nathan@null>
2017-08-23 06:04:22 +0100

    Disable cookies for performance

--- inventus_spider/settings.py
+++ inventus_spider/settings.py
@@ -33,7 +33,7 @@ DOWNLOAD_DELAY = 0.25
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 TELNETCONSOLE_ENABLED = False
